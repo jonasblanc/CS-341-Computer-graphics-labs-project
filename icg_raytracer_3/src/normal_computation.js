0644a94d4523fcb668e25198011bfd89cc063aa6
zabifade <zad.abifadel@epfl.ch>
2021-03-20 00:05:22 +0100

    mesh_shading1/desk2/desk3 are fully shown

--- icg_raytracer_3/src/normal_computation.js
+++ icg_raytracer_3/src/normal_computation.js
@@ -69,6 +69,15 @@ export function compute_vertex_normals(mesh, tri_normals, angle_weights) {
     const iv2 = mesh.tris.indices[3 * i_face + 1];
     const iv3 = mesh.tris.indices[3 * i_face + 2];
 
+
+
+    // vec3.add(vertex_normals[iv1], vertex_normals[iv1], tri_normals[i_face].map(function(x) { return x * angle_weights[i_face][0]; } ))
+    // vec3.add(vertex_normals[iv2], vertex_normals[iv2], tri_normals[i_face].map(function(x) { return x * angle_weights[i_face][1]; } ))
+    // vec3.add(vertex_normals[iv3], vertex_normals[iv3], tri_normals[i_face].map(function(x) { return x * angle_weights[i_face][2]; } ))
+    // vertex_normals[iv1] += tri_normals[i_face].map(function(x) { return x * angle_weights[i_face][0]; });
+    // vertex_normals[iv2] += tri_normals[i_face].map(function(x) { return x * angle_weights[i_face][1]; });
+    // vertex_normals[iv3] += tri_normals[i_face].map(function(x) { return x * angle_weights[i_face][2]; });
+
     // Add your code for adding the contribution of the current triangle to its vertices' normals
     vertex_normals[iv1][0] += tri_normals[i_face][0] * angle_weights[i_face][0];
     vertex_normals[iv1][1] += tri_normals[i_face][1] * angle_weights[i_face][0];
@@ -79,6 +88,10 @@ export function compute_vertex_normals(mesh, tri_normals, angle_weights) {
     vertex_normals[iv3][0] += tri_normals[i_face][0] * angle_weights[i_face][2];
     vertex_normals[iv3][1] += tri_normals[i_face][1] * angle_weights[i_face][2];
     vertex_normals[iv3][2] += tri_normals[i_face][2] * angle_weights[i_face][2];
+
+    // vertex_normals[iv1] += tri_normals[i_face] * angle_weights[i_face][0];
+    // vertex_normals[iv2] += tri_normals[i_face] * angle_weights[i_face][1];
+    // vertex_normals[iv3] += tri_normals[i_face] * angle_weights[i_face][2];
 
     /*
     vertex_normals[iv1] += tri_normals[i_face] * angle_weights[i_face][0];
