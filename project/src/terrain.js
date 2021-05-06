bfec82d5544d9383f78b6847c56b83103e335382
Loïc Houmard <loic.houmard@epfl.ch>
2021-05-06 17:48:43 +0200

    Add creation of cube if noise value >0

--- project/src/terrain.js
+++ project/src/terrain.js
@@ -30,6 +30,7 @@ class BufferData {
 function terrain_build_mesh(height_map) {
 	const grid_width = height_map.width
 	const grid_height = height_map.height
+	const grid_deepness = height_map.deepness //How to add that to height_map
 
 	const WATER_LEVEL = -0.03125
 
@@ -37,68 +38,102 @@ function terrain_build_mesh(height_map) {
 	const normals = []
 	const faces = []
 
-	// Map a 2D grid index (x, y) into a 1D index into the output vertex array.
-	function xy_to_v_index(x, y) {
-		return x + y*grid_width
+	// Map a 3D grid index (x, y, z) into a 1D index into the output vertex array.
+	function xyz_to_v_index(x, y, z) {
+		return x + y*grid_width+z*grid_width*grid_height
 	}
 
 	for(let gy = 0; gy < grid_height; gy++) {
 		for(let gx = 0; gx < grid_width; gx++) {
-			const idx = xy_to_v_index(gx, gy)
-			let elevation = height_map.get(gx, gy) - 0.5 // we put the value between 0...1 so that it could be stored in a non-float texture on older browsers/GLES3, the -0.5 brings it back to -0.5 ... 0.5
+			for(let gz = 0; gz < grid_deepness; gz++) {
 
-			// normal as finite difference of the height map
-			// dz/dx = (h(x+dx) - h(x-dx)) / (2 dx)
-			normals[idx] = vec3.normalize([0, 0, 0], [
-				-(height_map.get(gx+1, gy) - height_map.get(gx-1, gy)) / (2. / grid_width),
-				-(height_map.get(gx, gy+1) - height_map.get(gx, gy-1)) / (2. / grid_height),
-				1.,
-			])
+				const idx = xyz_to_v_index(gx, gy, gz)
+				//let visibility = height_map.get(gx, gy, gz) - 0.5 // we put the value between 0...1 so that it could be stored in a non-float texture on older browsers/GLES3, the -0.5 brings it back to -0.5 ... 0.5
 
-			/* TODO 6.1
-			Generate the displaced terrain vertex corresponding to integer grid location (gx, gy). 
-			The height (Z coordinate) of this vertex is determined by height_map.
-			If the point falls below WATER_LEVEL:
-			* it should be clamped back to WATER_LEVEL.
-			* the normal should be [0, 0, 1]
+				// normal as finite difference of the height map
+				// dz/dx = (h(x+dx) - h(x-dx)) / (2 dx)
+				normals[idx] = vec3.normalize([0, 0, 0], [
+					-(height_map.get(gx+1, gy, gz) - height_map.get(gx-1, gy, gz)) / (2. / grid_width),
+					-(height_map.get(gx, gy+1, gz) - height_map.get(gx, gy-1, gz)) / (2. / grid_height),
+					-(height_map.get(gx, gy, gz+1) - height_map.get(gx, gy, gz-1)) / (2. / grid_deepness),
+				])
 
-			The XY coordinates are calculated so that the full grid covers the square [-0.5, 0.5]^2 in the XY plane.
-			*/
-			const mapped_X = gx / grid_width - 0.5;
-			const mapped_Y = gy / grid_height - 0.5;
+				/* TODO 6.1
+				Generate the displaced terrain vertex corresponding to integer grid location (gx, gy). 
+				The height (Z coordinate) of this vertex is determined by height_map.
+				If the point falls below WATER_LEVEL:
+				* it should be clamped back to WATER_LEVEL.
+				* the normal should be [0, 0, 1]
 
-			if(elevation < WATER_LEVEL){
-				elevation = WATER_LEVEL;
-				normals[idx] = [0, 0, 1];
+				The XY coordinates are calculated so that the full grid covers the square [-0.5, 0.5]^2 in the XY plane.
+				*/
+				const mapped_X = gx / grid_width - 0.5;
+				const mapped_Y = gy / grid_height - 0.5;
+				const mapped_Z = gz / grid_deepness -0.5;
+
+				/*
+				if(visibility < WATER_LEVEL){
+					visibility = WATER_LEVEL;
+					normals[idx] = [0, 0, 1];
+				}
+				*/
+
+				//vertices[idx] = [mapped_X, mapped_Y, visibility]
+				vertices[idx] = [mapped_X, mapped_Y, mapped_Z]
+
 			}
-
-			vertices[idx] = [mapped_X, mapped_Y, elevation]
 		}
 	}
 
 	for(let gy = 0; gy < grid_height - 1; gy++) {
 		for(let gx = 0; gx < grid_width - 1; gx++) {
-			/* TODO 6.1
-			Triangulate the grid cell whose lower lefthand corner is grid index (gx, gy).
-			You will need to create two triangles to fill each square.
-			*/
+			for(let gz = 0; gz < grid_deepness - 1; gz++) {
 
-			const va = xy_to_v_index(gx, gy);
-			const vb = xy_to_v_index(gx + 1, gy);
-			const vc = xy_to_v_index(gx, gy + 1);
-			const vd = xy_to_v_index(gx + 1, gy + 1);
+				/* 
+				* We create a cube if the visibility is above 0 and nothing if it's below
+				*/
 
-			faces.push([va, vb, vc]);
-			faces.push([vb, vd, vc]);
+				let visibility = height_map.get(gx, gy, gz) - 0.5 // we put the value between 0...1 so that it could be stored in a non-float texture on older browsers/GLES3, the -0.5 brings it back to -0.5 ... 0.5
 
-			// faces.push([v1, v2, v3]) // adds a triangle on vertex indices v1, v2, v3
+				if(visibility >= 0){
+					const v000 = xyz_to_v_index(gx, gy, gz);
+					const v001 = xyz_to_v_index(gx, gy, gz+1);
+					const v010 = xyz_to_v_index(gx, gy + 1, gz);
+					const v011 = xyz_to_v_index(gx, gy + 1, gz+1);
+					const v100 = xyz_to_v_index(gx+1, gy, gz);
+					const v101 = xyz_to_v_index(gx+1, gy, gz+1);
+					const v110 = xyz_to_v_index(gx+1, gy + 1, gz);
+					const v111 = xyz_to_v_index(gx+1, gy + 1, gz+1);
+
+					faces.push([v000, v001, v010]);
+					faces.push([v011, v010, v001]);
+
+					faces.push([v000, v100, v001]);
+					faces.push([v101, v001, v100]);
+
+					faces.push([v000, v010, v100]);
+					faces.push([v110, v100, v010]);
+
+					faces.push([v111, v011, v101]);
+					faces.push([v001, v101, v011]);
+
+					faces.push([v111, v110, v011]);
+					faces.push([v010, v011, v110]);
+
+					faces.push([v111, v101, v110]);
+					faces.push([v100, v110, v101]);
+
+
+				// faces.push([v1, v2, v3]) // adds a triangle on vertex indices v1, v2, v3
+				}
+			}
 		}
-	}
 
-	return {
-		vertex_positions: vertices,
-		vertex_normals: normals,
-		faces: faces,
+		return {
+			vertex_positions: vertices,
+			vertex_normals: normals,
+			faces: faces,
+		}
 	}
 }
 
